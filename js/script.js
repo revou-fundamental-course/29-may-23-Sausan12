5d99dd653003b4c25e6fa5d56d06a2743921c6f4
sausan31 <21523052@students.uii.ac.id>
2023-06-08 16:48:13 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var text = "Hello World!";
+
+document.getElementById("kirim").addEventListener("click", function () {
+  console.log(text);
+});
+
+document.getElementsByName("input[name='nama']").addEventListener("input"),
+  function () {
+    console.log("sudah bisa diinput");
+  };
+
+function validateForm() {
+  const name = document.forms["message-form"]["full-name"].value;
+  const birthDate = document.forms["message-form"]["birth-date"].value;
+  const gender = document.forms["message-form"]["gender"].value;
+  const messages = document.forms["message-form"]["messages"].value;
+
+  if (name == "" || birthDate == "" || gender == "" || messages == "") {
+    alert("Tidak boleh ada yang kosong");
+    return false;
+  }
+
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthDate;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-messages").innerHTML = messages;
+  return false;
+  
+}
+
